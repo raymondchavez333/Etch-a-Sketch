143410cf8c7126d8e707303ab302e0bc33867c57
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-02-05 23:54:03 +0800

    Add a query selector for container div

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let box = document.createElement("div");
 box.setAttribute("class", "box");
 box.setAttribute("style", "width: 50px; height: 50px; background-color: green;");
 
+let container = document.querySelector(".container");
+
