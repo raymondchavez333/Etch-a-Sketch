6acbedb6b2e840961104efd51d2f74290efb6b0f
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-03-13 19:33:16 +0800

    Transferred all the codes within the for loop

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-let box = document.createElement("div");
-box.setAttribute("class", "box");
-box.setAttribute("style", "width: 50px; height: 50px;border-style: solid; border-color: black");
+// let box = document.createElement("div");
+// box.setAttribute("class", "box");
+// box.setAttribute("style", "width: 50px; height: 50px;border-style: solid; border-color: black");
 
-let container = document.querySelector(".container");
+// let container = document.querySelector(".container");
 
-let unorderedlist = document.createElement("ul");
+// let unorderedlist = document.createElement("ul");
 
-let listItem = document.createElement("li");
-listItem.appendChild(box);
+// let listItem = document.createElement("li");
+// listItem.appendChild(box);
 
 
 for (let i = 1; i <= 256; i++){
     
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    box.setAttribute("style", "width: 50px; height: 50px;border-style: solid; border-color: black");
+
+    let container = document.querySelector(".container");
+
+    let unorderedlist = document.createElement("ul");
+
+    let listItem = document.createElement("li");
+    listItem.appendChild(box);
+
     unorderedlist.appendChild(listItem);
+
+    container.appendChild(listItem);
 }
 
-container.appendChild(listItem);
+
 
 
