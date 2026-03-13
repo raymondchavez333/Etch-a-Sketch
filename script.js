cd2391cf0a0d9e4133c30ac07d0d225cf0ed824d
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-03-13 19:38:20 +0800

    Remove the list and simplified it just by using divs, use flex and flex wrap for the whole container

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// let box = document.createElement("div");
-// box.setAttribute("class", "box");
-// box.setAttribute("style", "width: 50px; height: 50px;border-style: solid; border-color: black");
-
-// let container = document.querySelector(".container");
-
-// let unorderedlist = document.createElement("ul");
-
-// let listItem = document.createElement("li");
-// listItem.appendChild(box);
-
-
 for (let i = 1; i <= 256; i++){
     
     let box = document.createElement("div");
@@ -18,16 +6,9 @@ for (let i = 1; i <= 256; i++){
 
     let container = document.querySelector(".container");
 
-    let unorderedlist = document.createElement("ul");
 
-    let listItem = document.createElement("li");
-    listItem.appendChild(box);
-
-    unorderedlist.appendChild(listItem);
-
-    container.appendChild(listItem);
+    container.appendChild(box);
 }
-
 
 
 
