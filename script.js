42af3030072952a4b8c6c49e86c0ba5bfc8e5f79
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-03-13 19:25:56 +0800

    Append the box into the listItem

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ let container = document.querySelector(".container");
 let unorderedlist = document.createElement("ul");
 
 let listItem = document.createElement("li");
+listItem.appendChild(box);
 
 
 for (let i = 1; i <= 256; i++){
     
-    container.appendChild(box);
+
+    unorderedlist.appendChild(listItem);
 }
 
 
