7e9cf80fa9960f665290816cd0a0d7f7df0e74b1
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-03-13 19:58:17 +0800

    Remove again the width and height properties of box

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ for (let i = 1; i <= 256; i++){
     
     let box = document.createElement("div");
     box.setAttribute("class", "box");
-    box.setAttribute("style", "width: 50px; height: 50px;border-style: solid; border-color: black");
+    box.setAttribute("style", "border-style: solid; border-color: black; display: flex; flex-shrink: 1");
 
+    // width: 50px; height: 50px;
     let container = document.querySelector(".container");
 
 
