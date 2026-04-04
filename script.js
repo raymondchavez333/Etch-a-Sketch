01ed74bf44fcde407159eed34db5199bf07632c6
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-04 16:57:21 +0800

    change the scope of newContainer variable which is outside of the for loop

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ button.addEventListener("click", () => {
     let container = document.querySelector(".container");
     container.remove();
 
+    let newContainer = document.createElement("div");
+    newContainer.classList("container");
+    
     for (let i = 1; i <= number; i++){
     
     const newWidth = 550 / number;
@@ -36,10 +39,6 @@ button.addEventListener("click", () => {
     let box = document.createElement("div");
     box.setAttribute("class", "box");
     box.setAttribute("style", `width: ${newWidth}px; height: auto; border-style: solid; border-color: black; flex-grow: 1`);
-
-    // width: 50px; height: 50px;
-    let newContainer = document.createElement("div");
-    newContainer.classList("container");
 
 
     newContainer.appendChild(box);
