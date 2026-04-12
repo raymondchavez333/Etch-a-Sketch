edcfb68770bd03f209d384fa20784db060f54eea
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-12 15:33:14 +0800

    Edited the dividedent of newWidth into 600

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ button.addEventListener("click", () => {
 
     for (let i = 1; i <= number*number; i++){
     
-    let newWidth = 550 / (number*number);
+    let newWidth = 600 / (number*number);
      
     let box = document.createElement("div");
     box.setAttribute("class", "box");
