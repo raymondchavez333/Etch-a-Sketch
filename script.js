62541fe60a718bb5151fc2bd66179b9c3a9cb7ec
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-14 00:38:48 +0800

    Set the background color based form randomized rgb colors

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ button.addEventListener("click", () => {
 
     boxes.forEach((box) =>{
         box.addEventListener("mouseenter", () => {
-            box.setAttribute("style", `width: ${newWidth}px; height: auto; border-style: solid; border-color: black; box-sizing: border-box; flex-grow: 1; background-color: black`);
+            box.setAttribute("style", `width: ${newWidth}px; height: auto; border-style: solid; border-color: black; box-sizing: border-box; flex-grow: 1; background-color: rgb(${randomB} ${randomG} ${randomG})`);
         });
     })
     }
